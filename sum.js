ff51d7a7e3439160f3f0ca0b187763b7b0d1eceb
Tomas Pasiecznik <42984839+tomaspasie@users.noreply.github.com>
2020-02-23 01:46:45 -0500

    Add files via upload

--- sum.js
+++ sum.js
@@ -1,5 +1,12 @@
 // Addition
-function sum(a,b){
+function sum(a,b = null){
+    var arraySum = 0;
+    if (Array.isArray(a)){
+        for (num in a){
+            arraySum+=a[num];
+        }
+        return arraySum;
+    }
     return a+b;
 }
 
